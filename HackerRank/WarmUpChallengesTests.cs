17e92907a8b15e9cc42174f3f283e745b295797c
benavidesarias <benavidesarias@gmail.com>
2019-11-13 06:48:57 -0500

    WarmUpChallenges

---
+++ HackerRank/WarmUpChallengesTests.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using Solutions;
+
+namespace HackerRank
+{
+    [TestFixture]
+    class WarmUpChallengesTests
+    {
+
+        [Test]
+        public void SockMerchant()
+        {
+            int[] ar = new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+            int n = 9;
+            int result = WarmUpChallenges.SockMerchant(n, ar);
+
+            int expected = 3;
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void CountingValleys()
+        {
+            //Test Case 1
+            int n = 8;
+            string s = "UDDDUDUU";
+            int result = WarmUpChallenges.CountingValleys(n, s);
+
+            int expected = 1;
+            Assert.AreEqual(expected, result);
+
+
+            //Test Case 2
+            n = 12;
+            s = "DDUUDDUDUUUD";
+            result = WarmUpChallenges.CountingValleys(n, s);
+
+            expected = 2;
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void JumpingOnTheClouds()
+        {
+            //Test Case 1
+            int[] c = new int[] { 0, 0, 1, 0, 0, 1, 0 };
+            int result = WarmUpChallenges.JumpingOnClouds(c);
+
+            int expected = 4;
+            Assert.AreEqual(expected, result);
+
+
+            //Test Case 2
+            c = new int[] { 0, 0, 0, 0, 1, 0 };
+            result = WarmUpChallenges.JumpingOnClouds(c);
+
+            expected = 3;
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [Test]
+        public void RepeatedString()
+        {
+            //Test Case 1
+            string s = "aba";
+            long n = 10;
+            long result = WarmUpChallenges.RepeatedString(s, n);
+
+            long expected = 7;
+            Assert.AreEqual(expected, result);
+
+            //Test Case 2
+            s = "a";
+            n = 1000000000000;
+            result = WarmUpChallenges.RepeatedString(s, n);
+
+            expected = 1000000000000;
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Solutions/WarmUpChallenges.cs
@@ -0,0 +1,104 @@
+﻿using System.Linq;
+
+namespace Solutions
+{
+    public class WarmUpChallenges
+    {
+        //problem #1
+        public static int SockMerchant(int n, int[] ar)
+        {
+            int c = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (ar[i] == ar[j] && ar[j] != -1)
+                    {
+                        c++;
+                        ar[i] = -1;
+                        ar[j] = -1;
+                        break;
+                    }
+                }
+
+            }
+
+            return c;
+        }
+
+        //Problem #2
+        public static int CountingValleys(int n, string s)
+        {
+            var level = 0;
+            var c = 0;
+
+            foreach (var step in s)
+            {
+                if (step == 'D')
+                {
+                    level--;
+                }
+                else
+                {
+                    if (level + 1 == 0)
+                    {
+                        c++;
+                    }
+
+                    level++;
+                }
+
+            }
+
+            return c;
+        }
+
+        //Problem #3
+        public static int JumpingOnClouds(int[] c)
+        {
+            int n = c.Length;
+            int s = 0;
+
+            for (int i = 0; i < n;)
+            {
+                if (i + 2 < n && c[i + 2] == 0)
+                {
+                    i += 2;
+                }
+                else
+                {
+                    i++;
+                }
+                s++;
+            }
+
+            return s - 1;
+        }
+
+        //Problem #4
+        public static long RepeatedString(string s, long n)
+        {
+            long total = 0;
+            long length = s.Length;
+
+            if (n > length)
+            {
+                long count = s.Count(x => x == 'a');
+                long integerPart = n / length;
+                long numberOfGroup = count * integerPart;
+
+                long rest = n - integerPart * length;
+                string subS = s.Substring(0, (int)rest);
+                long countSub = subS.Count(x => x == 'a');
+
+                total = numberOfGroup + countSub;
+            }
+            else
+            {
+                total = s.Substring(0, (int)n).Count(x => x == 'a');
+            }
+
+            return total;
+        }
+    }
+}
